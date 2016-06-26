ffbc319f0643e8aefb75de5b574ae8bda6051d42
EnderCaster <mstext0@hotmail.com>
2016-06-26 22:31:53 +0800

    20160626

--- dataStructure_SubjectDesign/dataStructure_SubjectDesign.cpp
+++ dataStructure_SubjectDesign/dataStructure_SubjectDesign.cpp
@@ -1,6 +1,6 @@
 // dataStructure_SubjectDesign.cpp : 定义控制台应用程序的入口点。
 //
-/*24.产品进销存管理系统（限1 人完成）（顺序或链式存储）
+/*24.产品进销存管理系统（限1 人完成）（链式存储）
 问题描述：针对某一种行业的库房的产品进销存情况进行管理。
 基本要求：
 1.	采用链式结构对库房的货品及其数量进行分类管理；
@@ -12,6 +12,28 @@
 #include <iostream>
 
 using namespace std;
+typedef struct Product
+{
+	string proNO;
+	string name;
+	int count;
+	string proKind;
+
+
+}product;
+typedef struct LNode
+{
+	LNode *pre;
+	product data;
+	LNode *next;
+}LNode;
+typedef struct Len {
+
+}len;
+void programInit() {
+	//从文件读数据，加载至内存
+	//初始化
+}
 int main()
 {
     return 0;
